328271ed6730168f3b3b6d3f4c0b40abde9bb11f
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2026-07-17 15:42:46 +0200

    Initial commit - AppLovin MAX Adapter 13.6.2.0

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "AppodealAppLovinMAXAdapter",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "AppodealAppLovinMAXAdapter",
+            targets: ["AppodealAppLovinMAXAdapterWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/appodeal/Appodeal-Swift-Package.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.6.2"),
+    ],
+    targets: [
+        .target(
+            name: "AppodealAppLovinMAXAdapterWrapper",
+            dependencies: [
+                .product(name: "Appodeal", package: "Appodeal-Swift-Package"),
+                .product(name: "AppLovinSDK", package: "AppLovin-MAX-Swift-Package"),
+                .target(name: "AppodealAppLovinMAXAdapter"),
+            ],
+            path: "Sources",
+            sources: ["Exports.swift"]
+        ),
+        .binaryTarget(
+            name: "AppodealAppLovinMAXAdapter",
+            url: "https://s3-us-west-1.amazonaws.com/appodeal-ios/Appodeal/SPM/AppodealAppLovinMAXAdapter/13.6.2.0/AppodealAppLovinMAXAdapter.xcframework.zip",
+            checksum: "6e917809aede94f9c428ced579c01413974a13960e869402eeaa7b711f2f5188"
+        ),
+    ]
+)
