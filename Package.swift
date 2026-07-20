e3f51975b569ff18c862a2645402f5c61b99362e
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2026-07-20 15:47:56 +0200

    Update Appodeal dependency to 4.3.0+

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["AppodealAppLovinMAXAdapterWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/appodeal/Appodeal-Swift-Package.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/appodeal/Appodeal-Swift-Package.git", .upToNextMajor(from: "4.3.0")),
         .package(url: "https://github.com/AppLovin/AppLovin-MAX-Swift-Package.git", exact: "13.6.2"),
     ],
     targets: [
